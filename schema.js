9e165cb09da23ab026383380e6977b50127a46bc
sonuchaurasiya7633 <sonukumar763303@gmail.com>
2025-07-19 12:30:39 +0530

    push

--- schema.js
+++ schema.js
@@ -1,12 +1,12 @@
 const Joi = require("joi");
 
 module.exports.postSchema = Joi.object({
-    post: Joi.object({
-        title: Joi.string().required(),
-        content: Joi.string().required(),
-        code: Joi.string().required(),
-        likes: Joi.number().min(0)
-    }).required(),
+  post: Joi.object({
+    title: Joi.string().required(),
+    content: Joi.string().required(),
+    code: Joi.string().required(),
+    likes: Joi.array().items(Joi.string().regex(/^[0-9a-fA-F]{24}$/))
+  }).required(),
 });
 
 module.exports.commentSchema = Joi.object({
